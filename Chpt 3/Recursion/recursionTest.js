226e412b05006fc0efc1961b4ed52e2a68e216d7
spacescript <simonbengoehring@gmail.com>
2021-06-07 20:53:53 -0700

    recursion

---
+++ Chpt 3/Recursion/recursionTest.js
@@ -0,0 +1,10 @@
+function isEven(n) {
+    if (n == 0) return true; 
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(99));
+console.log(isEven(-13));
