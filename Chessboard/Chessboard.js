ee3091667d434f0eafde6aad00177455de1dc415
spacescript <simonbengoehring@gmail.com>
2021-06-07 20:51:54 -0700

    Create Chessboard.js

---
+++ Chessboard/Chessboard.js
@@ -0,0 +1,20 @@
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+        if ((x + y) % 2 == 0) {
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n";
+}
+
+if chunk <= 22 function() {
+ console.log(12 % 5) < 3;
+}
+
+console.log(board);
